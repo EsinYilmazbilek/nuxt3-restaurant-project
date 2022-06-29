b05f18d8a4c355ceec5a5c3a7cee0ad3b2e25582
Esin Yilmazbilek <esinyilmazbilek@gmail.com>
2022-06-29 13:18:19 +0100

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <Html :lang="dynamic > 50  ? 'en-GB' : 'eng-US'">
+      <Head>
+        <Title>Restaurantly</Title>
+        <Meta 
+          name="description"
+          content="restaurant guide"
+        />
+        <Link 
+          rel="preload" 
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
---
+++ pages/restaurants/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Html :lang="dynamic > 50  ? 'en-GB' : 'eng-US'">
+      <Head>
+        <Title>Top 50 Restaurants</Title>
+        <Meta 
+          name="description"
+          content="restaurant guide"
+        />
+        <Link 
+          rel="preload" 
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+      </Head>
+    </Html>
+
+    <NuxtLayout name="custom">
+      <div class="container">
+        <RestaurantTable />
+      </div>
+    </NuxtLayout>
+  </div>
+</template>
